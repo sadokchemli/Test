42f4a6dfcc33d57a736abfdfeec61fbaf4203162
Sadok Chemli <sadok.chemli@yahoo.com>
2020-01-26 23:03:40 +0100

    test

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		static void Main(string[] args)
+		{
+			try
+			{
+				WebServiceReference.WebServiceSoapClient obj = new WebServiceReference.WebServiceSoapClient();
+
+				log.Info("Appel du web service Fibonacci(10)");
+
+				int fibonacci = obj.Fibonacci(10);
+				Console.WriteLine("FIBONACCI (10) : " + fibonacci);
+
+				log.Info("Appel du web service XmlToJson() de l'exemple 1");
+
+				string json1 = obj.XmlToJson("<foo>bar</foo>");
+				Console.WriteLine("XmlToJson exemple 1: \n" + json1);
+
+				log.Info("Appel du web service XmlToJson() de l'exemple 2");
+
+				string json2 = obj.XmlToJson("<foo>hello</bar>");
+				Console.WriteLine("XmlToJson exemple 2: \n" + json2);
+
+				log.Info("Appel du web service XmlToJson() de l'exemple 3");
+
+				string json3 = obj.XmlToJson("<TRANS><HPAY><ID>103</ID><STATUS>3</STATUS><EXTRA><IS3DS>0</IS3DS><AUTH>031183</AUTH></EXTRA><INT_MSG/><MLABEL>501767XXXXXX6700</MLABEL><MTOKEN>project01</MTOKEN></HPAY></TRANS>");
+				Console.WriteLine("XmlToJson exemple 3: \n" + json3);
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Une erreur est survenue");
+				log.Error(e.Message);
+			}
+
+			Console.Read();
+		}
+	}
+}
---
+++ Test/WebService.asmx.cs
@@ -0,0 +1,71 @@
+﻿using log4net;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+
+namespace Test
+{
+	/// <summary>
+	/// Description résumée de WebService
+	/// </summary>
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[System.ComponentModel.ToolboxItem(false)]
+	// Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante. 
+	// [System.Web.Script.Services.ScriptService]
+	public class WebService : System.Web.Services.WebService
+	{
+
+
+		ILog logger = log4net.LogManager.GetLogger("ErrorLog");
+		[WebMethod]
+		public int Fibonacci(int n)
+		{
+			try
+			{
+				int a = 0;
+				int b = 1;
+				if (n == 0) return 0;
+				else if (n == 1) return 1;
+
+				else if (n < 1 || n > 100) return -1;
+
+				for (int i = 0; i < n; i++)
+				{
+					int temp = a;
+					a = b;
+					b = temp + b;
+				}
+				return a;
+			}
+			catch (Exception e)
+			{
+				logger.Error(e.Message);
+				return -1;
+			}
+		}
+
+		[WebMethod]
+		public string XmlToJson(string xml)
+		{
+			XmlDocument doc = new XmlDocument();
+			string json = String.Empty;
+
+			try
+			{
+				doc.LoadXml(xml);
+				json = JsonConvert.SerializeXmlNode(doc);
+			}
+			catch (Exception e)
+			{
+				logger.Error(e.Message);
+				return "Bad Xml format";
+			}
+			return json;
+		}
+	}
+}
